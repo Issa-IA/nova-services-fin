67d38946c9512627e81a000a0866516115fc4a8e
Issa-IA <80253673+Issa-IA@users.noreply.github.com>
2022-12-20 12:14:18 +0100

    Update __manifest__.py

--- crmteamobjectif/__manifest__.py
+++ crmteamobjectif/__manifest__.py
@@ -13,7 +13,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale','crm'],
+    'depends': ['base','sale','crm','contacts'],
 
     # always loaded
     'data': [
