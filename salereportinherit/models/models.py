3b8a00c86bddb8383cf357cedcf3b0ea2cd2fa30
Issa-IA <80253673+Issa-IA@users.noreply.github.com>
2022-12-21 10:14:06 +0100

    Update models.py

--- salereportinherit/models/models.py
+++ salereportinherit/models/models.py
@@ -10,8 +10,7 @@ class SaleReportHerit(models.Model):
 
     def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
         fields['sale_marge_report'] = ", SUM(s.sale_comer) AS sale_marge_report"
-        fields['sale_chifre_report'] = ", SUM(s.sale_chifre_aff) AS sale_chifre_report"
-        fields['sale_contrat_report'] = ", SUM(s.sale_N_contrat) AS sale_contrat_report"
+        fields['sale_chifre_report'] = ", SUM(s.sale_chifre_aff) AS sale_chifre_report"        
         return super(SaleReportHerit, self)._query(with_clause, fields, groupby, from_clause)
 
 
