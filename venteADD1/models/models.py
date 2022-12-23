a1e36aa54a03d02007c89c592179eb42bc29c400
Issa-IA <80253673+Issa-IA@users.noreply.github.com>
2022-12-23 10:38:43 +0100

    Update models.py

--- venteADD1/models/models.py
+++ venteADD1/models/models.py
@@ -335,7 +335,7 @@ class SaleOrderHerit(models.Model):
             if rec.sale_maintnance:
                 rec.sale_marge = 0
             else:
-                rec.sale_marge = rec.sale_finance-rec.sale_frais_restitution_1-rec.sale_frais_livraison_new_1 - rec.sale_frais_livraison_new- rec.sale_total_rachat-rec.sale_montatnt_IR + rec.sale_frais -rec.sale_total_vente- rec.sale_frais_restitution -rec.sale_vr_client-rec.sale_ir_prospects-rec.sale_vr_client_2-rec.sale_rachat_matriel-rec.sale_Gratuite-rec.sale_partenariat-rec.sale_solde_2_fois
+                rec.sale_marge = rec.sale_finance - rec.sale_frais_livraison_new- rec.sale_total_rachat-rec.sale_montatnt_IR + rec.sale_frais -rec.sale_total_vente- rec.sale_frais_restitution -rec.sale_vr_client-rec.sale_ir_prospects-rec.sale_vr_client_2-rec.sale_rachat_matriel-rec.sale_Gratuite-rec.sale_partenariat-rec.sale_solde_2_fois
             
     @api.onchange("sale_frais_livraison_new","sale_total_rachat","sale_montatnt_IR","sale_total_achat", "sale_finance", "sale_frais", "sale_frais_restitution", "sale_vr_client",
                   "sale_ir_prospects", "sale_vr_client_2", "sale_rachat_matriel", "sale_Gratuite", "sale_partenariat",
